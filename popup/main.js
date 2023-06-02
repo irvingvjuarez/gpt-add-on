3a799e19b1a54dc1662fea916ff1a6deff11ddb7
Irving Juárez <irvingjuarez274@gmail.com>
2023-06-01 19:09:30 -0500

    [UPDATE] view and interaction of the page

--- popup/main.js
+++ popup/main.js
@@ -1,23 +1,30 @@
+import { isTargetSubmitButton, isTargetTitle } from "./utils/isTargetElement.js"
+import Element from "./controllers/element.js"
+
 (function() {
-  const headerTitle = document.querySelector(".header_title")
+  const chatTitleWrapper = document.querySelector(".chat-title-wrapper")
+  const textTitle = chatTitleWrapper.children[1]
+  const inputTitle = chatTitleWrapper.children[0]
+
   const chatForm = document.querySelector(".chat-input-wrapper")
 
-  function isTargetTitle(target) {
-    return target.tagName === "H2" 
-      && Boolean( target.className.match(/header_title/i) )
-  }
+  function toggleTitleInput() {
+    const hideClass = "hidden"
 
-  function isTargetSubmitButton(target) {
-    return target.parentElement.tagName === "svg"
-      && target.tagName === "path"
+    if ( Element.isVisible(textTitle) ) {
+      Element.addClass(textTitle, hideClass)
+      Element.removeClass(inputTitle, hideClass)
+    } else {
+      Element.addClass(inputTitle, hideClass)
+      Element.removeClass(textTitle, hideClass)
+    }
   }
 
   function clickListener(evt) {
     const target = evt.target
 
     if ( isTargetTitle(target) ) {
-      // TODO: handleChangingTitle()
-      console.log("handleChangingTitle")
+      toggleTitleInput()
     } else if ( isTargetSubmitButton(target) ) {
       // TODO: handleSubmitMessage()
       console.log("handleSubmitMessage")
